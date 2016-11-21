4b4b06753b5148c3248698d61fb70621819ad3d4
fuguoqiang <fuguoqiang@100tal.com>
2016-11-21 10:38:40 +0800

    初始化工程

---
+++ TALNetWorking/TALNetWorking.h
@@ -0,0 +1,19 @@
+//
+//  TALNetWorking.h
+//  TALNetWorking
+//
+//  Created by lmonster on 2016/11/21.
+//  Copyright © 2016年 lmonster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TALNetWorking.
+FOUNDATION_EXPORT double TALNetWorkingVersionNumber;
+
+//! Project version string for TALNetWorking.
+FOUNDATION_EXPORT const unsigned char TALNetWorkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TALNetWorking/PublicHeader.h>
+
+
